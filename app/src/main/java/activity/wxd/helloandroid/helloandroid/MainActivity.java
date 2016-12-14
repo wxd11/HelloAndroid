3faa4e4f939484d57089a7c1bdb0a8db815d4633
wxd <wxd11@gmail.com>
2016-12-14 19:39:11 +0800

    1.创建了button，并设置了点击事件，弹出toast 2.菜单

--- app/src/main/java/activity/wxd/helloandroid/helloandroid/MainActivity.java
+++ app/src/main/java/activity/wxd/helloandroid/helloandroid/MainActivity.java
@@ -2,12 +2,29 @@ package activity.wxd.helloandroid.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                      Toast.makeText(MainActivity.this,"toast",Toast.LENGTH_LONG);
+                      break;
+            default:break;
+        }
     }
 }
